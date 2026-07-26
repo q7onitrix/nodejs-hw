eb75dd8ddcc52be0f1e2ebdb80ed2849db3a9ef0
Onitrix <andriyproshak17@gmail.com>
2026-07-26 21:26:15 +0200

    Fix authentication and notes search

--- src/models/note.js
+++ src/models/note.js
@@ -1,35 +1,34 @@
-import { model, Schema } from "mongoose";
-import { TAGS } from "../constants/tags.js";
+import { model, Schema } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
-const noteSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  content: {
-    type: String,
-    default: '',
-    required: false,
-    trim: true,
-  },
-  tag: {
-    type: String,
-    enum: TAGS,
-    default: 'Todo',
-    required: false,
-  },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: TAGS,
+      default: 'Todo',
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
   {
     timestamps: true,
   },
 );
 
-noteSchema.index({ title: "text", content: "text"});
+noteSchema.index({ tag: 1, userId: 1 });
 
 export const Note = model('Note', noteSchema);
